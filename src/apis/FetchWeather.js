2cf93c03fa3b206442446be085b824635ecb8181
RickyTicky <Rickyabrooks92@gmail.com>
2022-06-10 01:24:38 -0400

    added current location finder for weather app

--- src/apis/FetchWeather.js
+++ src/apis/FetchWeather.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function FetchWeather() {
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
+
+    console.log("Latitude is:", lat);
+    console.log("Longitude is:", long);
+  }, [lat, long]);
+
   const apiGet = () => {
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?lat=26.934139&lon=-80.099739&appid=ae93a8df4eb012916dd53498f4b2cc0a"
+      `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ae93a8df4eb012916dd53498f4b2cc0a`
     )
       .then((response) => response.json())
       .then((data) => console.log(data));
