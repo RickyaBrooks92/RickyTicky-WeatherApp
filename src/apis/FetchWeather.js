a48c31f2ffcc579c7c3841fe9ae03117af27e053
RickyTicky <Rickyabrooks92@gmail.com>
2022-06-14 00:18:50 -0400

    weather app update

--- src/apis/FetchWeather.js
+++ src/apis/FetchWeather.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 function FetchWeather() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(function (position) {
@@ -14,12 +15,15 @@ function FetchWeather() {
     console.log("Longitude is:", long);
   }, [lat, long]);
 
-  const apiGet = () => {
-    fetch(
+  const apiGet = async () => {
+    await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=ae93a8df4eb012916dd53498f4b2cc0a`
     )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((res) => res.json())
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      });
   };
 
   return (
@@ -27,6 +31,7 @@ function FetchWeather() {
       My API Weather
       <br />
       <button onClick={apiGet}> Fetch Weather Data</button>
+      <p>{data.name}</p>
     </div>
   );
 }
