923b53dd4c08a0cea90b2e23d168607148d4023b
RickyTicky <Rickyabrooks92@gmail.com>
2022-07-08 21:54:50 -0400

    removed some files

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,23 @@
-import React, { useState } from "react";
-import "../App.css";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import CitySearch from "./CitySearch";
-import FetchFlights from "./FetchFlights";
+import React, { useState } from 'react';
+import '../App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import CitySearch from './CitySearch';
+
 export const DataContext = React.createContext();
 export const CenterContext = React.createContext();
+
 const Navigation = () => {
   const [weatherData, setWeatherData] = useState({});
   const [flightData, setFlightData] = useState({});
+
   const Center = {
-    fontFamily: "Arial",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    flexDirection: "column",
+    fontFamily: 'Arial',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
   };
+
   return (
     <DataContext.Provider
       value={{ weatherData, setWeatherData, flightData, setFlightData }}
@@ -22,14 +25,12 @@ const Navigation = () => {
       <CenterContext.Provider value={{ Center }}>
         <Router>
           <div>
-            <nav className="Nav">
-              <Link to={"/"}>Current Weather</Link>
-              <Link to={"/flights"}>Flights</Link>
+            <nav className='Nav'>
+              <Link to={'/'}>Current Weather</Link>
+              <Link to={'/flights'}>Flights</Link>
             </nav>
             <Routes>
-              <Route exact path="/" element={<CitySearch />} />
-              <Route exact path="/flights" element={<FetchFlights />} />
-              {/* <Route exact path="/flights" element={<AllFlights />} /> */}
+              <Route exact path='/' element={<CitySearch />} />
             </Routes>
           </div>
         </Router>
