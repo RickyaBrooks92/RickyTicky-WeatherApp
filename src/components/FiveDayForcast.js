00777d51950d59ffa5605a7dab515ea8d4bc3bd5
RickyTicky <Rickyabrooks92@gmail.com>
2022-08-15 20:51:24 -0400

    cleaned up the 5 day forcast and removed old code

--- src/components/FiveDayForcast.js
+++ src/components/FiveDayForcast.js
@@ -8,21 +8,48 @@ const FiveDayForcast = () => {
   const { fiveDayData } = React.useContext(DataContext);
 
   return (
-    <Box display='flex' justifyContent='center' alignItems='center'>
+    <Box
+      display='flex'
+      justifyContent='center'
+      alignItems='center'
+      alignContent='center'
+    >
       {fiveDayData.map((data) => (
         <Box m={2}>
-          {console.log(
-            new Date(data.dt_txt.substring(0, data.dt_txt.lastIndexOf(' ')))
-          )}
-          <Card sx={{ minWidth: 250 }}>
-            <Typography sx={{ fontSize: 14, textAlign: 'center' }}>
-              {data.dt_txt}
-            </Typography>
-            <Typography variant='h5' sx={{ textAlign: 'center' }}>
-              {data.main.feels_like} &deg;F
-            </Typography>
-            <Typography sx={{ textAlign: 'center', fontSize: 18 }}>
-              {data.weather[0].main}
+          <Card
+            sx={{
+              minWidth: 250,
+
+              border: 1,
+              borderRadius: '16px',
+              maxHeight: 250,
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'center',
+            }}
+          >
+            <Typography sx={{ textAlign: 'center' }}>
+              <div Style='font-size: 20px; font-weight: bold; font-family: "Lucida Console", "Courier New", monospace;'>
+                {new Date(data.dt_txt).toLocaleString('default', {
+                  month: 'short',
+                  year: 'numeric',
+                  day: 'numeric',
+                })}
+              </div>
+              <div Style='font-size: 20px; font-weight: bold; font-family: "Lucida Console", "Courier New", monospace;'>
+                {data.main.feels_like} &deg;F
+              </div>
+              <img
+                src={`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+                alt='weather icon'
+                width='90'
+                height='90'
+                align='center'
+              ></img>
+              <div Style='font-size: 16px; font-weight: bold; font-family: "Lucida Console", "Courier New", monospace;'>
+                {data.weather[0].main}
+              </div>
+              {console.log(data)}
             </Typography>
           </Card>
         </Box>
